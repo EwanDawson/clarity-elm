31eb65676308bdd29a7c4e16eea9d4395156a960
Ewan Dawson <Ewan.Dawson@SymphonicTrust.com>
2017-03-29 13:50:02 +0100

    Reorder declarations

--- Clarity.elm
+++ Clarity.elm
@@ -5,6 +5,9 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
 
+-- containers
+
+
 type alias MainContainer model msg =
     { appAlert : model -> Html msg
     , header : model -> Html msg
@@ -17,6 +20,28 @@ type alias ContentContainer model msg =
     { contentArea : model -> Html msg
     , sidenav : model -> Html msg
     }
+
+
+mainContainer : MainContainer model msg -> model -> Html msg
+mainContainer layout model =
+    div [ class "main-container" ]
+        [ div [ class "alert alert-app-level" ] [ layout.appAlert model ]
+        , header [ class "header header-6" ] [ layout.header model ]
+        , nav [ class "subnav" ] [ layout.subnav model ]
+        , div [ class "content-container" ] [ layout.contentContainer model ]
+        ]
+
+
+contentContainer : ContentContainer model msg -> model -> Html msg
+contentContainer layout model =
+    div [ class "content-container" ]
+        [ div [ class "content-area" ] [ layout.contentArea model ]
+        , nav [ class "sidenav" ] [ layout.sidenav model ]
+        ]
+
+
+
+-- alerts
 
 
 type alias Alert msg =
@@ -76,39 +101,6 @@ alertClass alert =
            )
 
 
-mainContainer : MainContainer model msg -> model -> Html msg
-mainContainer layout model =
-    div [ class "main-container" ]
-        [ div [ class "alert alert-app-level" ] [ layout.appAlert model ]
-        , header [ class "header header-6" ] [ layout.header model ]
-        , nav [ class "subnav" ] [ layout.subnav model ]
-        , div [ class "content-container" ] [ layout.contentContainer model ]
-        ]
-
-
-contentContainer : ContentContainer model msg -> model -> Html msg
-contentContainer layout model =
-    div [ class "content-container" ]
-        [ div [ class "content-area" ] [ layout.contentArea model ]
-        , nav [ class "sidenav" ] [ layout.sidenav model ]
-        ]
-
-
-icon : String -> List (Attribute msg) -> Html msg
-icon shape attrs =
-    node "clr-icon" ((attribute "shape" shape) :: attrs) []
-
-
-p : Int -> List (Attribute msg) -> List (Html msg) -> Html msg
-p n attrs content =
-    Html.p ((class ("p" ++ (toString n))) :: attrs) content
-
-
-closeButton : msg -> Html msg
-closeButton message =
-    button [ type_ "button", class "close", onClick message ] [ icon "close" [] ]
-
-
 appAlert : Alert msg -> Html msg
 appAlert alert =
     div [ class ((alertClass alert) ++ " alert-app-level") ]
@@ -141,3 +133,30 @@ alertItem item =
 alertAction : AlertAction msg -> Html msg
 alertAction action =
     a [ class "dropdown-item", onClick action.message ] [ text action.name ]
+
+
+
+-- icons
+
+
+icon : String -> List (Attribute msg) -> Html msg
+icon shape attrs =
+    node "clr-icon" ((attribute "shape" shape) :: attrs) []
+
+
+
+-- content
+
+
+p : Int -> List (Attribute msg) -> List (Html msg) -> Html msg
+p n attrs content =
+    Html.p ((class ("p" ++ (toString n))) :: attrs) content
+
+
+
+-- buttons
+
+
+closeButton : msg -> Html msg
+closeButton message =
+    button [ type_ "button", class "close", onClick message ] [ icon "close" [] ]
